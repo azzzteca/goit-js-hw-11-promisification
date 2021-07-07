5673b5f867a721d54b55d56005e2c98045e7333b
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-07-07 13:26:11 +0300

    part 1 of 3 done

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// import './sass/main.scss';
+
+const delay = ms => {
+  // Твой код
+  const promice = new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
+
+  return promice;
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
