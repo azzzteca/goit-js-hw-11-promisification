7cdeb16e5e1854d95cd49de42e53ae91a4573bd9
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-07-11 20:33:00 +0300

    all parts done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // import './sass/main.scss';
 
+// Подзадание 1
+
 const delay = ms => {
   // Твой код
-  const promice = new Promise(resolve => {
+  return new Promise(resolve => {
     setTimeout(() => {
       resolve(ms);
     }, ms);
   });
-
-  return promice;
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
@@ -17,3 +17,68 @@ const logger = time => console.log(`Resolved after ${time}ms`);
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+// Подзадание 2
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: false },
+];
+
+const toggleUserState = (allUsers, username) => {
+  const promise = new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === username ? { ...user, active: !user.active } : user,
+    );
+
+    resolve(updatedUsers);
+  });
+  return promise;
+};
+
+// Currently the function works like this
+// toggleUserState(users, 'Mango', console.table);
+// toggleUserState(users, 'Ajax', console.table);
+
+// The function should work like this
+toggleUserState(users, 'Mango').then(console.table);
+toggleUserState(users, 'Ajax').then(console.table);
+
+// Подзадание 3
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  const delay = randomIntegerFromInterval(200, 500);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve({ id: transaction.id, time: delay });
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
+};
+
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+// Currently the function works like this
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+
+// The function should work like this
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
